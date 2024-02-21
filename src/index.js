33704717de8bbb9b8918be5b41b26576092b4044
jackwong07 <61767751+jackwong07@users.noreply.github.com>
2024-02-21 23:18:07 +0000

    update

--- src/index.js
+++ src/index.js
@@ -5,23 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require('socket.io');
-const io = new Server(server);
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static('public'));
-
-io.on('connect', (socket) =>{
-  console.log('a user connected');
-  socket.on('disconnect', () =>{
-    console.log('a user disconnected');
-  })
-})
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
