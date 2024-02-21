84c61425cd18e267cb6dc2e61156792620edf807
jackwong07 <61767751+jackwong07@users.noreply.github.com>
2024-02-21 23:33:29 +0000

    update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const cors = require("cors");
+app.use(cors());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
